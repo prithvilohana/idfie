1fe185430649952fade5e58bd9fecef837c7e0ae
prithvilohana <prithvilohana750@gmail.com>
2024-10-21 14:55:00 +0500

    half front

--- src/main/java/com/example/idfie/HelloApplication.java
+++ src/main/java/com/example/idfie/HelloApplication.java
@@ -1,18 +1,24 @@
 package com.example.idfie;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("IDfie");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/idfie/HelloController.java
+++ src/main/java/com/example/idfie/HelloController.java
@@ -1,14 +1,65 @@
 package com.example.idfie;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+import javafx.scene.image.ImageView;
 
 public class HelloController {
+
+    public Button IdGenerator;
     @FXML
-    private Label welcomeText;
+    private Label noFIle;
+
+    public Button generateBtn;
+    @FXML
+    private ImageView userPicture;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void generateBtnClick(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File("c:\\"));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JPEG image", "*.jpg"), new FileChooser.ExtensionFilter("PNG image", "*.png"));
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+        if(selectedFile != null){
+            Image image = new Image(selectedFile.getPath());
+            userPicture.setImage(image);
+        }
+        else {
+            noFIle.setText("No file is selected");
+        }
     }
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void generateID(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("IDcard.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("generateID() method called");
+        if (root == null) {
+            System.out.println("FXML file not found!");
+        } else {
+            System.out.println("FXML file loaded successfully");
+        }
+    }
+
+
+    @FXML
+    private ImageView profileImage;
 }
